9c03f8b6727c88f205d860838044d0ad86653256
fuyc <fuyc@jingoal.com>
2016-01-15 12:24:04 +0800

    clean up
    Change-Id: If249f147d691dd861f83f3b4acb48a864db13d87

--- dubbo-service/run.sh
+++ dubbo-service/run.sh
@@ -1,5 +1,6 @@
 
-export MAVEN_OPTS="-Dcom.sun.management.jmxremote.port=27777 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.local.only=false"
+# enable jmx on demand
+# export MAVEN_OPTS="-Dcom.sun.management.jmxremote.port=27777 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.local.only=false"
 
 # mvn test
 mvn exec:java -Dexec.mainClass=com.jingoal.test.business.ServerMain
